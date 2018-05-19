c576f712a08930c9e625aa40cf422d611da13cea
lokuhra <leandro.88.bernardi@gmail.com>
2018-05-19 10:44:11 -0300

    add material-ui

--- components/Head/index.js
+++ components/Head/index.js
@@ -6,6 +6,7 @@ export default ({ title }) => (
     <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
     <link rel="stylesheet" href="../../static/style.css" />
 
   </Head>
